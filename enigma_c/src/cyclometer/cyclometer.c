a273f6c5b5c623c8f6e16d55a4716ef645c4e2b5
Arif Hasanic <arif@hasanic.com>
2024-03-20 00:11:03 +0100

    create cycle counts

--- enigma_c/src/cyclometer/cyclometer.c
+++ enigma_c/src/cyclometer/cyclometer.c
@@ -1,11 +1,14 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "../helper/helper.h"
 #include "cyclometer.h"
 #include "enigma/enigma.h"
-#include "enigma/plugboard/plugboard.h"
+
+#define TOTAL_CYCLES 26 * 26 * 26 * 3 * 2 * 1
+
+int possible_rotor_permutations[6][3] = {{1, 2, 3}, {1, 3, 2}, {2, 1, 3},
+                                         {2, 3, 1}, {3, 1, 2}, {3, 2, 1}};
 
 typedef struct CycleConfiguration
 {
@@ -18,17 +21,32 @@ typedef struct CycleConfiguration
 
 int ring_settings[3] = {0, 0, 0};
 
-void create_cycles_from_permutaion(int *permutaion, CycleOfRotorSetting *cycle,
-                                   int rotor)
+int get_cycle_count(int *permutaion)
 {
+    int number_of_cycles = 0;
+    int visted[26]       = {0};
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (visted[i] == 0)
+        {
+            number_of_cycles++;
+            int j = i;
+            while (visted[j] == 0)
+            {
+                visted[j] = 1;
+                j         = permutaion[j];
+            }
+        }
+    }
+
+    return number_of_cycles;
 }
 
-CycleOfRotorSetting *create_cycle(CycleConfiguration *cycle_configuration)
+CycleOfRotorSetting *create_cycle(CycleConfiguration *cycle_configuration,
+                                  int *rotor_permutation)
 {
     CycleOfRotorSetting *cycle = malloc(sizeof(CycleOfRotorSetting));
-    cycle->rotors[0]           = cycle_configuration->rotor_one;
-    cycle->rotors[1]           = cycle_configuration->rotor_two;
-    cycle->rotors[2]           = cycle_configuration->rotor_three;
 
     EnigmaConfiguration *configuration = malloc(sizeof(EnigmaConfiguration));
     configuration->type                = M3;
@@ -39,15 +57,20 @@ CycleOfRotorSetting *create_cycle(CycleConfiguration *cycle_configuration)
     configuration->rotor_positions[0] = cycle_configuration->rotor_one;
     configuration->rotor_positions[1] = cycle_configuration->rotor_two;
     configuration->rotor_positions[2] = cycle_configuration->rotor_three;
-    configuration->ring_settings[0]   = 0;
-    configuration->ring_settings[1]   = 0;
-    configuration->ring_settings[2]   = 0;
+    cycle->rotor_positions[0]         = cycle_configuration->rotor_one;
+    cycle->rotor_positions[1]         = cycle_configuration->rotor_two;
+    cycle->rotor_positions[2]         = cycle_configuration->rotor_three;
+
+    configuration->ring_settings[0] = 0;
+    configuration->ring_settings[1] = 0;
+    configuration->ring_settings[2] = 0;
     // FIXME: Rotor 1,1,1 should not be possible, values have to be unique
-    configuration->rotors[0] = cycle_configuration->rotor_permutation / 12 + 1;
-    configuration->rotors[1] =
-        (cycle_configuration->rotor_permutation / 3) % 4 + 1;
-    configuration->rotors[2] = cycle_configuration->rotor_permutation % 3 + 1;
-    configuration->reflector = cycle_configuration->reflector_index;
+    configuration->rotors[0] = rotor_permutation[0];
+    configuration->rotors[1] = rotor_permutation[1];
+    configuration->rotors[2] = rotor_permutation[2];
+    cycle->rotors[0]         = rotor_permutation[0];
+    cycle->rotors[1]         = rotor_permutation[1];
+    cycle->rotors[2]         = rotor_permutation[2];
 
     for (int i = 0; i < 26; i++)
     {
@@ -79,9 +102,9 @@ CycleOfRotorSetting *create_cycle(CycleConfiguration *cycle_configuration)
         free_enigma(enigma);
     }
 
-    create_cycles_from_permutaion(rotor_one_permutation, cycle, 0);
-    create_cycles_from_permutaion(rotor_two_permutation, cycle, 1);
-    create_cycles_from_permutaion(rotor_three_permutation, cycle, 2);
+    cycle->cycles_counts[0] = get_cycle_count(rotor_one_permutation);
+    cycle->cycles_counts[1] = get_cycle_count(rotor_two_permutation);
+    cycle->cycles_counts[2] = get_cycle_count(rotor_three_permutation);
 
     free(configuration->rotor_positions);
     free(configuration->rotors);
@@ -94,9 +117,8 @@ void create_cycles(void)
 {
     // 3 rotors and 26 possible settings for each rotor, 5 * 4 * 3 rotor
     // permutations, 2 reflectors
-    int total_cycles = 26 * 26 * 26 * 5 * 4 * 3 * 2;
     CycleOfRotorSetting **cycles =
-        malloc(total_cycles * sizeof(CycleOfRotorSetting));
+        malloc(TOTAL_CYCLES * sizeof(CycleOfRotorSetting));
     int iterations = 0;
 
     for (int rotor_one = 0; rotor_one < 26; rotor_one++)
@@ -105,33 +127,31 @@ void create_cycles(void)
         {
             for (int rotor_three = 0; rotor_three < 26; rotor_three++)
             {
-                for (int rotor_permutation = 0; rotor_permutation < 5 * 4 * 3;
+                for (int rotor_permutation = 0; rotor_permutation < 3 * 2 * 1;
                      rotor_permutation++)
                 {
-                    for (int reflector_index = 0; reflector_index < 2;
-                         reflector_index++)
-                    {
-                        CycleConfiguration *cycle_configuration =
-                            malloc(sizeof(CycleConfiguration));
-                        cycle_configuration->rotor_one   = rotor_one;
-                        cycle_configuration->rotor_two   = rotor_two;
-                        cycle_configuration->rotor_three = rotor_three;
-                        cycle_configuration->rotor_permutation =
-                            rotor_permutation;
-                        cycle_configuration->reflector_index =
-                            reflector_index + 'B';
-                        cycles[iterations] = create_cycle(cycle_configuration);
-                        iterations++;
-                    }
+
+                    CycleConfiguration *cycle_configuration =
+                        malloc(sizeof(CycleConfiguration));
+                    cycle_configuration->rotor_one         = rotor_one;
+                    cycle_configuration->rotor_two         = rotor_two;
+                    cycle_configuration->rotor_three       = rotor_three;
+                    cycle_configuration->rotor_permutation = rotor_permutation;
+                    cycle_configuration->reflector_index   = 'B';
+                    cycles[iterations]                     = create_cycle(
+                        cycle_configuration,
+                        possible_rotor_permutations[rotor_permutation]);
+                    iterations++;
                 }
             }
         }
     }
 
-    for (int i = 0; i < total_cycles; i++)
+    for (int i = 0; i < TOTAL_CYCLES; i++)
     {
         free(cycles[i]);
     }
 
-    printf("Total cycles: %d\n", iterations);
+    free(cycles);
+    printf("Total cycles: %d\n", TOTAL_CYCLES);
 }
--- enigma_c/src/cyclometer/cyclometer.h
+++ enigma_c/src/cyclometer/cyclometer.h
@@ -2,15 +2,9 @@
 
 typedef struct
 {
-    int cycle_count;
-    struct Cycle *next;
-} Cycle;
-
-typedef struct
-{
-    Cycle cycles[3];
-    char message_key[3];
+    int cycles_counts[3];
     int rotor_positions[3];
     int rotors[3];
 } CycleOfRotorSetting;
+
 void create_cycles(void);

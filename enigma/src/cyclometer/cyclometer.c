9bc130eab40c593aff52b435b46cfb769c198c11
Your Name <you@example.com>
2024-07-15 20:37:31 +0200

    code cleanup

--- enigma/src/cyclometer/cyclometer.c
+++ enigma/src/cyclometer/cyclometer.c
@@ -48,9 +48,27 @@ Cycle get_cycle_count(int *rotor_permutation)
         }
     }
 
+    // TODO: Sort the cycles earlier
+    for (int i = 0; i < cycle.length; i++)
+    {
+        for (int j = i + 1; j < cycle.length; j++)
+        {
+            if (cycle.cycle_values[i] < cycle.cycle_values[j])
+            {
+                int temp              = cycle.cycle_values[i];
+                cycle.cycle_values[i] = cycle.cycle_values[j];
+                cycle.cycle_values[j] = temp;
+            }
+        }
+    }
+
     return cycle;
 }
 
+/**
+ * @brief Print the cycle values
+ * @param cycle: Cycle to print
+ */
 void print_cycle(Cycle cycle)
 {
     printf("( ");
@@ -134,12 +152,7 @@ CycleOfRotorSetting *create_cycle(CycleConfiguration *cycle_configuration,
     print_cycle(cycle->cycles[1]);
     printf(" / ");
     print_cycle(cycle->cycles[2]);
-
-    printf(": %d %d %d: ", cycle_configuration->rotor_one,
-           cycle_configuration->rotor_two, cycle_configuration->rotor_three);
-    printf("%d %d %d\n", configuration->rotors[0], configuration->rotors[1],
-           configuration->rotors[2]);
-
+    printf("\n");
     free(configuration->rotor_positions);
     free(configuration->rotors);
     free(configuration);
@@ -150,7 +163,7 @@ CycleOfRotorSetting *create_cycle(CycleConfiguration *cycle_configuration,
 void create_cycles(void)
 {
     // 3 rotors and 26 possible settings for each rotor, 5 * 4 * 3 rotor
-    // permutations, 2 reflectors
+    // permutations
     CycleOfRotorSetting **cycles =
         malloc(TOTAL_CYCLES * sizeof(CycleOfRotorSetting));
     int iterations = 0;
